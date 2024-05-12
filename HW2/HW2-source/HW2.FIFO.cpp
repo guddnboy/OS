e8bfc77f8970286f5770ece910eb14beb0c9150b
guddnboy <hung3018@naver.com>
2024-05-12 18:41:14 +0900

    FIFO 코드에 대한 분석(주석달기)

--- HW2/HW2-source/HW2.FIFO.cpp
+++ HW2/HW2-source/HW2.FIFO.cpp
@@ -2,6 +2,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// 프레임의 수 만큼 반복 후 페이지가 테이블 프레임과 같으면 1 아니면 0 반환
 int present(int table_frame[], int nf, int page){
 	for(int i=0; i<nf; i++){
 		if(page == table_frame[i]){
@@ -11,6 +12,7 @@ int present(int table_frame[], int nf, int page){
 	return 0;
 }
 
+// 테이블 프레임 내용 출력 비어있으면 '--', 아니면 값 출력
 void printtable(int table_frame[], int nf){
 	for(int i=0; i<nf; i++){
 		if(table_frame[i] == -1){
@@ -28,17 +30,22 @@ int main(){
     //nf-number of frames
     int n,nf,i,pos=0;
 
+    // 프레임의 수 입력받기
     printf("enter number of frames\n");
     scanf("%d",&nf);
+
+    // 프레임의 수만큼 테이블 값 -1로 초기화
     int table_frame[nf];
     for(i=0;i<nf;i++){
         table_frame[i]=-1;
     }
 
+    // 페이지 요청의 수 입력받기
     printf("enter total number of page requests\n");
     scanf("%d",&n);
     int pages[n];
     printf("enter reference string\n");
+    // 요청할 페이지 입력
     for(i=0;i<n;i++){
         scanf("%d",&pages[i]);
     }
@@ -47,13 +54,19 @@ int main(){
     printf("position of frame table after each request\n");
     for(i=0;i<n;i++){
         printf("page table after request from %2d || ",pages[i]);
+        
+        //  요청 페이지와 테이블의 페이지가 일치하면 1을 반환, 일치하는 상황(!1)은 거짓이므로 패스
         if(!present(table_frame,nf,pages[i])){
-           table_frame[pos] = pages[i];
-           pos = (pos+1)%nf ;//considering it as a queue
-           printtable(table_frame,nf);
-           printf("page fault\n");
-           count1++;
-           continue;
+            // 마지막으로 입력한 인덱스에 요청한 페이지 추가 
+            table_frame[pos] = pages[i];
+            // 현재 인덱스+1 하고 프레임의 수 만큼 나눈 나머지 (인덱스를 순환시키기 위해서)
+            pos = (pos+1)%nf ;//considering it as a queue
+            printtable(table_frame,nf);
+            printf("page fault\n");
+
+            // 페이지 요청에 따라 추가되었으면 count1 1 증가시키기
+            count1++;
+            continue;
         }
         printtable(table_frame,nf);
 		printf("\n");
