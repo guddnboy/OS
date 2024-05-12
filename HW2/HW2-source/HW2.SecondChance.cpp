64276588a392cc1fc1781d77a98415cfca7bd413
guddnboy <hung3018@naver.com>
2024-05-12 17:30:33 +0900

    SecondChance 코드 보기 편하게 수정

--- HW2/HW2-source/HW2.SecondChance.cpp
+++ HW2/HW2-source/HW2.SecondChance.cpp
@@ -1,41 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int present(int a[],int nf,int page,int rf_bit[])
-{
-    for(int i=0;i<nf;i++)
-    {
-        if(a[i]==page)
-        {
+int present(int a[],int nf,int page,int rf_bit[]){
+    for(int i=0;i<nf;i++){
+        if(a[i]==page){
           rf_bit[i]=1;
           return 1;
         }
-
     }
     return 0;
 }
 
-void printtable(int table_frame[], int nf)
-{
-	for(int i=0; i<nf; i++)
-	{
-		if(table_frame[i] == -1)
+void printtable(int table_frame[], int nf){
+	for(int i=0; i<nf; i++){
+		if(table_frame[i] == -1){
 			printf("-- ");
-		else
+        }
+		else{
 			printf("%2d ", table_frame[i]);
+        }
 	}
 	printf("||");
 }
 
 
-int main()
-{
+int main(){
     int nf;
     cout<<"enter number of frames"<<endl;
     cin>>nf;
     int a[nf],rf_bit[nf];
-    for(int i=0;i<nf;i++)
-    {
+    for(int i=0;i<nf;i++){
         a[i]=-1;
         rf_bit[i]=0;
     }
@@ -45,21 +39,17 @@ int main()
     cin>>np;
     int b[np];
     cout<<"enter page requests"<<endl;
-    for(int i=0;i<np;i++)
-    {
+    for(int i=0;i<np;i++){
         cin>>b[i];
     }
     int pos=0,count1=0;
 
-    for(int i=0;i<np;i++)
-    {
+    for(int i=0;i<np;i++){
         printf("page table after request from %2d || ",b[i]);
-        if(!present(a,nf,b[i],rf_bit))
-        {
+        if(!present(a,nf,b[i],rf_bit)){
             //int pos=findpos(a,nf,b,i,np);
             pos=(pos+1)%nf;
-            while(rf_bit[pos]==1)
-            {
+            while(rf_bit[pos]==1){
                 rf_bit[pos]=0;
                 pos=(pos+1)%nf;
             }
