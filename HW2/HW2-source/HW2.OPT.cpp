4d6efaecd85fc63c80c5ba55c7f7ac1297a7ca7e
guddnboy <hung3018@naver.com>
2024-05-12 17:29:22 +0900

    OPT 코드 보기 편하게 수정

--- HW2/HW2-source/HW2.OPT.cpp
+++ HW2/HW2-source/HW2.OPT.cpp
@@ -1,43 +1,40 @@
 #include<bits/stdc++.h>
 
-int present(int table_frame[], int nf, int page)
-{
-	for(int i=0; i<nf; i++)
+int present(int table_frame[], int nf, int page){
+	for(int i=0; i<nf; i++){
 		if(page == table_frame[i])
 			return 1;
+    }
 	return 0;
 }
 
-void printtable(int table_frame[], int nf)
-{
-	for(int i=0; i<nf; i++)
-	{
-		if(table_frame[i] == -1)
+void printtable(int table_frame[], int nf){
+	for(int i=0; i<nf; i++){
+		if(table_frame[i] == -1){
 			printf("-- ");
-		else
+        }
+		else{
 			printf("%2d ", table_frame[i]);
+        }
 	}
 	printf("||");
 }
 
 
-int findpos(int table_frame[],int nf,int pages[],int curr,int np)
-{
+int findpos(int table_frame[],int nf,int pages[],int curr,int np){
     int i,j;
-    for(i=0;i<nf;i++)
-    {
-        if(table_frame[i] == -1)
+    for(i=0;i<nf;i++){
+        if(table_frame[i] == -1){
             return i;
+        }
     }
 
     int pos[nf]={0};
-    for(i=0;i<nf;i++)
-    {
+
+    for(i=0;i<nf;i++){
         pos[i]=1e9;
-        for(j=curr+1;j<np;j++)
-        {
-            if(pages[j]==table_frame[i])
-            {
+        for(j=curr+1;j<np;j++){
+            if(pages[j]==table_frame[i]){
                 pos[i]=j;
                 break;
             }
@@ -46,10 +43,8 @@ int findpos(int table_frame[],int nf,int pages[],int curr,int np)
 
     int max1=-1;
     int returnpos=-1;
-    for(i=0;i<nf;i++)
-    {
-        if(pos[i]>max1)
-        {
+    for(i=0;i<nf;i++){
+        if(pos[i]>max1){
             max1=pos[i];
             returnpos=i;
         }
@@ -58,16 +53,14 @@ int findpos(int table_frame[],int nf,int pages[],int curr,int np)
     return returnpos;
 }
 
-int main()
-{
+int main(){
     //nf-number of frames
     int n,nf,i,pos=0;
 
     printf("enter number of frames\n");
     scanf("%d",&nf);
     int table_frame[nf];
-    for(i=0;i<nf;i++)
-    {
+    for(i=0;i<nf;i++){
         table_frame[i]=-1;
     }
 
@@ -75,18 +68,15 @@ int main()
     scanf("%d",&n);
     int pages[n];
     printf("enter pages\n");
-    for(i=0;i<n;i++)
-    {
+    for(i=0;i<n;i++){
         scanf("%d",&pages[i]);
     }
 
     int count1=0;
     printf("position of frame table after each request\n");
-    for(i=0;i<n;i++)
-    {
+    for(i=0;i<n;i++){
         printf("page table after request from %2d || ",pages[i]);
-        if(!present(table_frame,nf,pages[i]))
-        {
+        if(!present(table_frame,nf,pages[i])){
              int pos = findpos(table_frame,nf,pages,i,n);
              table_frame[pos]=pages[i];
 
