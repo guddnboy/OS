f418f8a4cf0d40341078fbaf5cc78ca154d5e6c7
guddnboy <hung3018@naver.com>
2024-05-18 22:28:12 +0900

    2차기회페이지교체알고리즘 정수형을 문자형으로 변경

--- HW2/HW2-source/HW2.SecondChance.cpp
+++ HW2/HW2-source/HW2.SecondChance.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int present(int a[],int nf,int page,int rf_bit[]){
+int present(char a[],int nf,char page,int rf_bit[]){
     for(int i=0;i<nf;i++){
         if(a[i]==page){
           rf_bit[i]=1;
@@ -11,13 +11,13 @@ int present(int a[],int nf,int page,int rf_bit[]){
     return 0;
 }
 
-void printtable(int table_frame[], int nf){
+void printtable(char table_frame[], int nf){
 	for(int i=0; i<nf; i++){
-		if(table_frame[i] == -1){
+		if(table_frame[i] == ' '){
 			printf("-- ");
         }
 		else{
-			printf("%2d ", table_frame[i]);
+			printf("%c ", table_frame[i]);
         }
 	}
 	printf("||");
@@ -28,16 +28,17 @@ int main(){
     int nf;
     cout<<"enter number of frames"<<endl;
     cin>>nf;
-    int a[nf],rf_bit[nf];
+    char a[nf];
+    int rf_bit[nf];
     for(int i=0;i<nf;i++){
-        a[i]=-1;
+        a[i]= ' ';
         rf_bit[i]=0;
     }
 
     int np;
     cout<<"enter number of pages"<<endl;
     cin>>np;
-    int b[np];
+    char b[np];
     cout<<"enter page requests"<<endl;
     for(int i=0;i<np;i++){
         cin>>b[i];
@@ -45,7 +46,7 @@ int main(){
     int pos=0,count1=0;
 
     for(int i=0;i<np;i++){
-        printf("page table after request from %2d || ",b[i]);
+        printf("page table after request from %c || ",b[i]);
         if(!present(a,nf,b[i],rf_bit)){
             //int pos=findpos(a,nf,b,i,np);
             pos=(pos+1)%nf;
