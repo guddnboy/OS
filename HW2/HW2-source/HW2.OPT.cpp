6b12345112211d172739c54f1ad3d8c32f834eb4
guddnboy <hung3018@naver.com>
2024-05-20 15:48:56 +0900

    최적 교체 알고리즘 완성

--- HW2/HW2-source/HW2.OPT.cpp
+++ HW2/HW2-source/HW2.OPT.cpp
@@ -1,101 +1,91 @@
 #include<bits/stdc++.h>
+using namespace std;
 
-// 프레임의 수 만큼 반복 후 페이지가 테이블 프레임과 같으면 1 아니면 0 반환
 int present(char table_frame[], int nf, char page){
-	for(int i=0; i<nf; i++){
-		if(page == table_frame[i])
-			return 1;
+    for(int i = 0; i < nf; i++){
+        if(page == table_frame[i])
+            return 1;
     }
-	return 0;
+    return 0;
 }
 
-// 테이블 프레임 내용 출력 비어있으면 '--', 아니면 값 출력
 void printtable(char table_frame[], int nf){
-	for(int i=0; i<nf; i++){
-		if(table_frame[i] == ' '){
-			printf("-- ");
+    for(int i = 0; i < nf; i++){
+        if(table_frame[i] == ' '){
+            printf("-- ");
         }
-		else{
-			printf("%c  ", table_frame[i]);
+        else{
+            printf("%c  ", table_frame[i]);
         }
-	}
-	printf("||");
+    }
+    printf("||");
 }
 
+int findpos(char table_frame[], int nf, char pages[], int curr, int n){
+    int i, j;
 
-int findpos(char table_frame[],int nf,char pages[],int curr,int n){
-    int i,j;
-    // 테이블의 값이 비어있으면 해당 인덱스 반환
-    for(i=0;i<nf;i++){
+    for(i = 0; i < nf; i++){
         if(table_frame[i] == ' '){
             return i;
         }
     }
 
     int pos[nf];
-// 앞으로의 요청들 중에서 다시 호출되기까지의 시간이 긴 요청 찾기
-    for(i=0;i<nf;i++){
+    for(i = 0; i < nf; i++){
+        pos[i] = INT_MAX;
         for(j = curr + 1; j < n; j++){
             if(pages[j] == table_frame[i]){
-                pos[i]=j;
+                pos[i] = j;
                 break;
             }
-            else{
-                pos[i] = 1e9;
-            }
         }
     }
 
-// 값이 가장 큰 페이지, 교체할 공간을 결정
-    int max1=-1;
-    int returnpos=-1;
-    for(i=0;i<nf;i++){
-        if(pos[i]>max1){
-            max1=pos[i];
-            returnpos=i;
+    int max1 = -1, returnpos = -1;
+    for(i = 0; i < nf; i++){
+        if(pos[i] > max1){
+            max1 = pos[i];
+            returnpos = i;
         }
     }
-
     return returnpos;
 }
 
 int main(){
-    //nf-number of frames
-    int n,nf,i,pos=0;
+    int n, nf, i, pos = 0;
 
-    printf("enter number of frames\n");
-    scanf("%d",&nf);
+    printf("Enter number of frames\n");
+    scanf("%d", &nf);
     char table_frame[nf];
-    for(i=0;i<nf;i++){
-        table_frame[i]=' ';
+    for(i = 0; i < nf; i++){
+        table_frame[i] = ' ';
     }
 
-    printf("enter total number of page requests\n");
-    scanf("%d",&n);
+    printf("Enter total number of page requests\n");
+    scanf("%d", &n);
     char pages[n];
-    printf("enter pages\n");
-    for(i=0;i<n;i++){
-        scanf(" %c",&pages[i]);
+    printf("Enter pages\n");
+    for(i = 0; i < n; i++){
+        scanf(" %c", &pages[i]);
     }
 
-    int count1=0;
-    printf("position of frame table after each request\n");
-    for(i=0;i<n;i++){
-        printf("page table after request from %c || ",pages[i]);
-        if(!present(table_frame,nf,pages[i])){
-             int pos = findpos(table_frame,nf,pages,i,n);
-             table_frame[pos]=pages[i];
+    int count1 = 0;
+    printf("Position of frame table after each request\n");
+    for(i = 0; i < n; i++){
+        printf("Page table after request from %c || ", pages[i]);
+        if(!present(table_frame, nf, pages[i])){
+            int pos = findpos(table_frame, nf, pages, i, n);
+            table_frame[pos] = pages[i];
 
-             printtable(table_frame,nf);
-             printf("page fault\n");
-             count1++;
-             continue;
+            printtable(table_frame, nf);
+            printf(" page fault\n");
+            count1++;
+        } else {
+            printtable(table_frame, nf);
+            printf("\n");
         }
-
-
-        printtable(table_frame,nf);
-		printf("\n");
-
     }
-    printf("\nNumber of page faults : %d\n\n", count1);
+    printf("\nNumber of page faults: %d\n\n", count1);
+
+    return 0;
 }
