b417f17bab9fbe89e9c4587ffa8d334c42e68009
guddnboy <hung3018@naver.com>
2024-05-27 09:56:58 +0900

    [SSTF] 코드 정리

--- HW3/HW3-source/HW3.SSTF.cpp
+++ HW3/HW3-source/HW3.SSTF.cpp
@@ -1,9 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-
+int main(){
     int head;
     printf("enter head position\n");
     cin>>head;
@@ -13,8 +11,8 @@ int main()
     cin>>n;
     int queue1[n],done[n];
     printf("enter requests\n");
-    for(i=0;i<n;i++)
-    {
+
+    for(i=0;i<n;i++){
         scanf("%d",&queue1[i]);
         done[i]=0;
     }
@@ -23,30 +21,27 @@ int main()
 
     int temp1=head;
     int count1=0,j,sum=0;
-    while(count1!=n)
-    {
-    int temp=100000;
-    int k=-1;
-    for(j=0;j<n;j++)
-    {
-        if(temp1!=queue1[j] && done[j]==0)
-        {
-        if(temp>abs(temp1-queue1[j]))
-        {
-           temp = abs(temp1-queue1[j]);
-           k = j;
-        }
-        }
-    }
-    sum+=temp;
-    printf("%d->",queue1[k]);
-    count1++;
-    temp1=queue1[k];
-    done[k]=1;
-    //cout<<temp1<<endl;
-    }
-    cout<<endl;
-    cout<<"total head movement is="<<sum<<endl;
 
+    while(count1!=n){
+        int temp=100000;
+        int k=-1;
 
+        for(j=0;j<n;j++){
+            if(temp1!=queue1[j] && done[j]==0){
+                if(temp>abs(temp1-queue1[j])){
+                    temp = abs(temp1-queue1[j]);
+                    k = j;
+                }
+            }
+        }
+        
+        sum+=temp;
+        printf("%d->",queue1[k]);
+        count1++;
+        temp1=queue1[k];
+        done[k]=1;
+        //cout<<temp1<<endl;
+        cout<<endl;
+        cout<<"total head movement is="<<sum<<endl;
+    }
 }
